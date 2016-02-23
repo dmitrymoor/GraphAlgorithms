be165a6c27ea1d2046bf0a988f3bef056d8ef1b6
Dmitry <Dmitry@Lenovo-PC>
2016-02-23 15:16:56 +0100

    first

---
+++ GraphAlgorithms/src/main/java/ch/uzh/ifi/GraphAlgorithms/App.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.GraphAlgorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
